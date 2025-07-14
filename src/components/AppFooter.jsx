671e042768491a80fe250c4d5da04faa277e5a42
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-14 17:42:50 +0200

    fixati colori

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -1,6 +1,10 @@
 
 import logoBG from "./../img/dc-logo-bg.png"
 import facebook from "./../img/footer-facebook.png"
+import twitter from "./../img/footer-twitter.png"
+import youtube from "./../img/footer-youtube.png"
+import pinterest from "./../img/footer-pinterest.png"
+import periscope from "./../img/footer-periscope.png" 
 export default function Footer() {
     return (
         <footer>
@@ -63,16 +67,16 @@ export default function Footer() {
           </div>
         </div>
             <div className="downFooter d-flex justify-content-between align-items-center">
-                <div className="container d-flex justify-content-between align-items-center">
-                    <div><a href="">SIGN-UP NOW!</a></div>
-                <div className="social d-flex">
-                        <div><h4>FOLLOW US</h4></div>
+                <div className="container d-flex justify-content-between align-items-center ">
+                    <div><a className="sign p-2" href="">SIGN-UP NOW!</a></div>
+                <div className="social d-flex gap-3">
+                        <div><h4 className="text_blue">FOLLOW US</h4></div>
                     <ul className="  list-inline">
                             <li className="list-inline-item"><a href=" "><img src={facebook} alt="" /></a> </li>
-                            <li className="list-inline-item"><a href=" "><img src={facebook} alt="" /></a> </li>
-                            <li className="list-inline-item"><a href=" "><img src={facebook} alt="" /></a> </li>
-                            <li className="list-inline-item"><a href=" "><img src={facebook} alt="" /></a> </li>
-                                <li className="list-inline-item"><a href=" "><img  src={facebook} alt="" /></a> </li>
+                            <li className="list-inline-item"><a href=" "><img src={twitter} alt="" /></a> </li>
+                            <li className="list-inline-item"><a href=" "><img src={youtube} alt="" /></a> </li>
+                            <li className="list-inline-item"><a href=" "><img src={pinterest} alt="" /></a> </li>
+                            <li className="list-inline-item"><a href=" "><img  src={periscope} alt="" /></a> </li>
                     </ul>
                 </div>
                 </div>

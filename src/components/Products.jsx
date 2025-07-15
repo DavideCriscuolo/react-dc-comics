349c663d05b35afbdf8d0296ccfed84fd4a8e8de
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-15 12:34:03 +0200

    fixato aspetto img comics

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,12 +4,14 @@ import comics from "./../data/comics"
 
 export default function Products(){
     return (
-        <div className="container p-5">
+        <section>
+            <h4 className="title_prod p-3">CURRENT SERIES</h4>
+                <div className="container p-5">
             <div className="row  row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-6">
                 {comics.map((comic) => {
                     return <div  key= {comic.id} className="col p-3">
-                        <div className="card h-100 "> 
-                            <img className="card-img-top  img-fluid w-100 object-fit-fill w-100 " src={comic.thumb} alt="Title" />
+                        <div className="card  "> 
+                            <img className=" comicImg w-100 ratio ratio-1x1  " src={comic.thumb} alt="Title" />
                             <div className="card-body">
                                 <h4 className="card-title fs-6">{comic.title}</h4>
                             </div>
@@ -23,6 +25,9 @@ export default function Products(){
 
        
    </div>
+        
+        </section>
+
 
 
 )

449b713725fa7d0a9b2ae5337914c84d92d0670c
DavideCriscuolo <davide.alcatel@gmail.com>
2025-07-15 15:45:39 +0200

    BONUS fixato il footer dinamico e creato file contenente i dati

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -3,45 +3,29 @@ import twitter from "./../img/footer-twitter.png";
 import youtube from "./../img/footer-youtube.png";
 import pinterest from "./../img/footer-pinterest.png";
 import periscope from "./../img/footer-periscope.png";
-
-const listDc = [
-  {
-    id: 1,
-    title: "Characters",
-  },
-  {
-    id: 2,
-    title: "Comics",
-  },
-  {
-    id: 3,
-    title: "Movies",
-  },
-  {
-    id: 4,
-    title: "Tv",
-  },
-  {
-    id: 5,
-    title: "Games",
-  },
-  {
-    id: 6,
-    title: "Videos",
-  },
-  {
-    id: 7,
-    title: "News",
-  },
-];
-
+import { listDc } from "../data/listFooter";
+import { listDcComics } from "../data/listFooter";
+import { listShop } from "../data/listFooter";
+import { listSites } from "../data/listFooter";
 export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <div className="row p-4 row-cols-4">
+        <div className="row p-4 row-cols-3">
           <div className="col">
-            <h5 className="text-white">Dc Comics</h5>
+            <h5 className="text-white">DC COMICS</h5>
+            <ul>
+              {listDcComics.map((item) => {
+                return (
+                  <li key={item.id}>
+                    <a href="">{item.title}</a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="col">
+            <h5 className="text-white">DC</h5>
             <ul>
               {listDc.map((item) => {
                 return (
@@ -53,9 +37,9 @@ export default function Footer() {
             </ul>
           </div>
           <div className="col">
-            <h5 className="text-white">Dc Comics</h5>
+            <h5 className="text-white">SITES</h5>
             <ul>
-              {listDc.map((item) => {
+              {listSites.map((item) => {
                 return (
                   <li key={item.id}>
                     <a href="">{item.title}</a>
@@ -65,33 +49,9 @@ export default function Footer() {
             </ul>
           </div>
           <div className="col">
-            <h5 className="text-white">Dc Comics</h5>
+            <h5 className="text-white">SHOP</h5>
             <ul>
-              {listDc.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <a href="">{item.title}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="col">
-            <h5 className="text-white">Dc Comics</h5>
-            <ul>
-              {listDc.map((item) => {
-                return (
-                  <li key={item.id}>
-                    <a href="">{item.title}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-          <div className="col">
-            <h5 className="text-white">Dc Comics</h5>
-            <ul>
-              {listDc.map((item) => {
+              {listShop.map((item) => {
                 return (
                   <li key={item.id}>
                     <a href="">{item.title}</a>
---
+++ src/data/listFooter.js
@@ -0,0 +1,111 @@
+export const listDcComics = [
+  {
+    id: 1,
+    title: "Characters",
+  },
+  {
+    id: 2,
+    title: "Comics",
+  },
+  {
+    id: 3,
+    title: "Movies",
+  },
+  {
+    id: 4,
+    title: "Tv",
+  },
+  {
+    id: 5,
+    title: "Games",
+  },
+  {
+    id: 6,
+    title: "Videos",
+  },
+  {
+    id: 7,
+    title: "News",
+  },
+];
+
+export const listShop = [
+  {
+    id: 1,
+    title: "Shop DC",
+  },
+  {
+    id: 2,
+    title: "Shop Dc Collectibles",
+  },
+];
+
+export const listDc = [
+  {
+    id: 1,
+    title: "Terms of Use",
+  },
+  {
+    id: 2,
+    title: "Privacy policy (New)",
+  },
+  {
+    id: 3,
+    title: "And Choices",
+  },
+  {
+    id: 4,
+    title: "Advertising",
+  },
+  {
+    id: 5,
+    title: "Jobs",
+  },
+  {
+    id: 6,
+    title: "Subscription",
+  },
+  {
+    id: 7,
+    title: "Talent Workspace",
+  },
+  {
+    id: 8,
+    title: "CPSC Certificates Workspace",
+  },
+  {
+    id: 9,
+    title: "Ratings",
+  },
+  {
+    id: 10,
+    title: "Shop Help",
+  },
+  {
+    id: 11,
+    title: "Contact Us",
+  },
+];
+
+export const listSites = [
+  {
+    id: 1,
+    title: "DC",
+  },
+  {
+    id: 2,
+    title: "MAD Magazin",
+  },
+  {
+    id: 3,
+    title: "DC Kids",
+  },
+  {
+    id: 4,
+    title: "DC Universe",
+  },
+  {
+    id: 5,
+    title: "DC Power Visa",
+  },
+];
